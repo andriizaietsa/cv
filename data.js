9a69a73d26736614f3aaaeedc5a851b4a8e2f54d
zaiets.andrii <zaiets.andrii@globaldev.tech>
2025-05-13 22:15:14 +0300

    remove all data

--- data.js
+++ data.js
@@ -1,36 +1,26 @@
 const resumeData = {
-    name: "Andrii Zaiets",
-    email: "zaietsandriia@gmail.com",
-    phone: "380675762887",
-    linkedin: "https://www.linkedin.com/in/andrii-zaiets-74348b208/",
-    profileSummary: "QA Engineer with over 4 years of experience in web and mobile testing. Strong background in automation, manual testing, and CI tools. Skilled in delivering quality products across financial, education, travel, and sports domains.",
+    name: "",
+    email: "",
+    phone: "",
+    linkedin: "",
+    profileSummary: "",
     workExperience: [
       {
-        title: "QA Engineer",
-        company: "Globaldev Group",
-        location: "Remote, Ukraine",
-        period: "May 2022 – Present",
+        title: "",
+        company: "",
+        location:"",
+        period: "",
         details: [
-          "Worked on projects across sports, education, travel, and financial systems.",
-          "Tested both web and mobile applications.",
-          "Used Postman, DevTools, Charles Proxy, and Git for daily QA tasks.",
-          "Developed automation tests from scratch using Cypress.",
-          "Performed UI testing with DevTools and designs in Figma.",
-          "Integrated tests with GitLab CI."
+         
         ]
       },
       {
-        title: "QA Engineer",
-        company: "WizardsDev",
+        title: "",
+        company: "",
         location: "Dnipro, Ukraine",
-        period: "March 2021 – April 2022",
+        period: "",
         details: [
-          "Worked on QA for financial systems.",
-          "Performed functional testing of web applications.",
-          "Tested APIs using Postman",
-          "Validated data using SQL queries.",
-          "Reported bugs with detailed steps and logs.",
-          "Used TestRail for test case management."
+         
         ]
       }
     ]
